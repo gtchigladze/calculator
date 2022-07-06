07ada652ac2e93498a6ac569356816f1a4a99e03
Mariam Gasparian <gasparian.websmart@gmail.com>
2022-07-06 15:47:23 +0400

    add display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DisplayModule } from './display/display.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    DisplayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
