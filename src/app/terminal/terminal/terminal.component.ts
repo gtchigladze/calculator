b373c3fb46bede9f549eec10eafbc3bb2d856fa9
Giorgi Tchigladze <tchigladze.websmart@gmail.com>
2022-07-07 12:34:55 +0400

    update

--- src/app/terminal/terminal/terminal.component.ts
+++ src/app/terminal/terminal/terminal.component.ts
@@ -10,26 +10,41 @@ export class TerminalComponent implements OnInit {
 
   operation = '25 * 2'
 
-  
 
   constructor(private service: CalculatorServiceService) { }
 
-  // value: string = '1+2';
+  value: string[] = ['22'];
 
   parseOperation(operation: string) {
-    console.log(operation);
+    
 
     const parsetExpresstion = this.parseExpression(operation);
+
+    console.log(parsetExpresstion);
 
     this.service.calculateExpression(parsetExpresstion)
     
   }
 
   parseExpression(expression: string): string[] {
-    return ['1', '+', '1'];
+ 
+
+
+    // let x = ['1', '+', '1']
+
+    // const result =x.reduce((acc:any, curr:any) => acc[0] + curr[2])
+    // console.log(result)
+    // return ['1', '+', '1'];
+    return [expression[0], expression[1], expression[2]]
+
+   
   }
 
+  
+
   ngOnInit(): void {
+
+
   }
 // getTerminalNumber(){
 //   this.service.addResultArray(this.value)
