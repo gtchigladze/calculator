667a019b14f0956385812eb0c0a5675e610534dd
Mariam Gasparian <gasparian.websmart@gmail.com>
2022-07-08 11:28:00 +0400

    new feature: keyboard

---
+++ src/app/keyboard/keyboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    KeyboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class KeyboardModule { }
